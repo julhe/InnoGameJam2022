20186c2bb276efc0efdbaf17b4df25e69a925907
Julian Heinken <hi@julianheinken.com>
2022-11-11 22:47:38 +0100

    🎉

---
+++ Assets/Scripts/ScatterObjectDefinition.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScatterObjectDefinition : ScriptableObject
+{
+    public GameObject Prefab;
+    float NoiseAmplitude = 1.0f, NoiseFrequency = 1.0f, NoiseBias = 0.0f;
+    
+    
+}
---
+++ Assets/Scripts/TerrainPlane.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class TerrainPlane : MonoBehaviour
+{
+    public ScatterObjectDefinition[] ObjectDefinitions;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        foreach (ScatterObjectDefinition scatterObject in ObjectDefinitions)
+        {
+            
+        }
+    }
+}
